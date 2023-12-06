3323f275e4713bf9ce46e77a2636002a49cabd58
Ajay Duraisamy <ajayduraisamy@gmail.com>
2023-12-06 10:20:00 +0530

    Added keyboard input support

--- app.js
+++ app.js
@@ -15,3 +15,26 @@ function calculate(){
         display.value = "Error";
     }
 }
+
+// Keyboard support
+document.addEventListener("keydown", (e) => {
+
+    const allowedKeys = "0123456789/*-+.";
+
+    if(allowedKeys.includes(e.key)){
+        press(e.key);
+    }
+
+    if(e.key === "Enter"){
+        e.preventDefault();
+        calculate();
+    }
+
+    if(e.key === "Backspace"){
+        display.value = display.value.slice(0,-1);
+    }
+
+    if(e.key === "Escape"){
+        clearDisplay();
+    }
+});
